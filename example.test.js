dbf2eac13a5fd20ca993ec4055140ebf122beac9
OmarSafi1 <86169999+OmarSafi1@users.noreply.github.com>
2024-12-19 17:46:10 +0100

    Create example.test.js

---
+++ example.test.js
@@ -0,0 +1,8 @@
+// test/example.test.js
+const assert = require('assert');
+
+describe('Example Test', function() {
+  it('should return true', function() {
+    assert.strictEqual(true, true);
+  });
+});
